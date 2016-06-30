2e045146c863152629cc581d0a1ff29d12034a47
glp <1058719078@qq.com>
2016-06-30 17:57:59 +0800

    新增Glide 图片加载库，修改网络访问权限等等，对按钮进行更多的操作

--- app/src/main/java/com/example/administrator/test/MainActivity.java
+++ app/src/main/java/com/example/administrator/test/MainActivity.java
@@ -4,7 +4,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ListView;
+
+import com.bumptech.glide.Glide;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -12,15 +15,21 @@ import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
+    @BindView(R.id.img) ImageView mImg;
     @BindView(R.id.editText) EditText mEditText;
     @BindView(R.id.list) ListView mList;
 
     RewardListAdapter mAdapter;
 
+    static boolean mIsShow = true;
+
     @OnClick(R.id.button) void onClickBtn(){
         int count = Integer.parseInt("0"+mEditText.getText().toString());
         mAdapter.setCount(count);
         mAdapter.notifyDataSetChanged();
+
+        mIsShow = !mIsShow;
+        mImg.setVisibility(mIsShow?View.VISIBLE:View.GONE);
     }
 
     @Override
@@ -29,6 +38,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_test);
 
         ButterKnife.bind(this);
+
+        Glide.with(this)
+                .load("http://img3.duitang.com/uploads/item/201406/24/20140624172202_JfevR.jpeg")
+                //.centerCrop()
+                .placeholder(R.mipmap.de)
+                .crossFade()
+                .into(mImg);
 
         //mList = (ListView) findViewById(R.id.list);
 
